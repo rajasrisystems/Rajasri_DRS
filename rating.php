585d725b9861cf2cd0113c72ddf3b6d6bf49d1ef
Rajasri Systems <businessdev@rajasri.net>
2016-08-02 19:43:49 +0530

    Add files via upload

--- rating.php
+++ rating.php
@@ -5,9 +5,18 @@
 	$objLogin->chklogin();
 	$objSmarty->assign('activePage',"2");
 	if($_REQUEST['Hitaction']=='1')
-		{
+	{
 		$objLogin->insertdetails();
-		}
+	}
+	if($_REQUEST['update_rating']=='1')
+	{
+		$objLogin->Update_rating($_REQUEST['Rat_Id']);
+	}
+	if($_REQUEST['Rat_Id']!='')
+	{
+		$objLogin->get_RatingID($_REQUEST['Rat_Id']);
+	}
+	$objLogin->display();
 	$disvar = "SELECT * FROM resource";
 	$result = mysql_query($disvar) OR die(mysql_error());
 	while($row = mysql_fetch_assoc($result))
--- report.php
+++ report.php
@@ -1,17 +1,15 @@
 <?php 
 	include 'includes/common.php';
 	include_once "includes/classes/class.index.php";
+	include_once "includes/classes/class.report.php";
 	$objLogin = new Login();
+	
 	$objLogin->chklogin();
-	$resourcevar = "SELECT * FROM resource";
-	$resresult = mysql_query($resourcevar) OR die(mysql_error());
-	while($resrow = mysql_fetch_assoc($resresult))
-		{
-			$resdata[] = $resrow; // store in array	
-		}
-	$objSmarty->assign('resdata', $resdata);	
-	//$objReport = new Login();
-	//$objReport->rptpage();
+	$objReport = new Report();
+	$objReport->getAllResources();
+	$objReport->getAllRating();
+	//$objReport->slctdisp();
+	$objSmarty->assign('objReport',$objReport);
 	$objSmarty->assign('activePage',"3");
 	$objSmarty->assign('IncludeTpl',"report.tpl");
 	$objSmarty->display("pagetemplate.tpl");
